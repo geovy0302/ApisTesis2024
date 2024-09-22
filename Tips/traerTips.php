7383f3b0508a85b81289d2ce1f954896321a5917
Geovy0302 <geovy032002@gmail.com>
2024-09-22 04:31:45 -0500

    Cambio completo de las APIs de Tips

--- Tips/traerTips.php
+++ Tips/traerTips.php
@@ -11,7 +11,7 @@ require_once("conexion.php"); //Inclusión requerida el archivo de conexión
 
     $response = array();//se crea un arreglo vacío para almacenar los resultados de la consulta
     
-    $Id_tips = $_POST['Id_tips'];
+    $Id_tips = $_GET['Id_tips'];
   
     $querys = $con->prepare("SELECT Descripcion_Tip FROM  mensaje_tips  where  Id_Mensaje_Tips = ?");		
 	$querys->execute(array($Id_tips));
@@ -25,11 +25,17 @@ require_once("conexion.php"); //Inclusión requerida el archivo de conexión
             array_push($response, $stuff);
         }
         if($response != null){
+            $stuff = array();
+            $stuff["process"] = "Datos_de_tips_encontrados";
+            $stuff["message"] = "Datos de tips encontrado";
+            array_push($response, $stuff);
             header('Content-Type: application/json');
             echo (json_encode($response));
             exit();
         }else{
-            $response["success"] = "Tips No Encontrado";
+
+            $response["process"]= "Datos_de_tips_no_encontrados";
+            $response["message"] = "Datos de tips no encontrado";
             header('Content-Type: application/json');
             echo (json_encode($response));
             exit();
